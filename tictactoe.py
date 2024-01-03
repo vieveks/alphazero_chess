946fcd4992ea31f03959a3246759a0339e860986
vieveks <vivekp.padman@gmail.com>
2024-01-03 19:05:38 +0530

    changes to the tictactoe

--- tictactoe.py
+++ tictactoe.py
@@ -39,3 +39,34 @@ class TicTacToe:
     
     def get_opponent(self, player):
         return -player
+    
+if __name__ == "__main__":
+    tictactoe = TicTacToe()
+    player = 1
+
+    state = tictactoe.get_initial_state()
+
+    while True:
+        print(state)
+        valid_moves = tictactoe.get_valid_moves(state)
+        print("valid_moves", [i for i in range(tictactoe.action_size) if valid_moves[i] == 1])
+        action = int(input(f"{player}:"))
+
+        if valid_moves[action] == 0:
+            print("action not valid")
+            continue
+            
+        state = tictactoe.get_next_state(state, action, player)
+        
+        value, is_terminal = tictactoe.get_value_and_terminated(state, action)
+        
+        if is_terminal:
+            print(state)
+            if value == 1:
+                print(player, "won")
+            else:
+                print("draw")
+            break
+            
+        # Switch player for the next turn
+        player = tictactoe.get_opponent(player)
